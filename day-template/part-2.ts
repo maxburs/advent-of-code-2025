d74e4fab7be53f3b846699d448ca7b49ef872af0
Maxwell Burson <maxwellburson@gmail.com>
2025-12-06 05:14:08 -0800

    tweak template

--- day-template/part-2.ts
+++ day-template/part-2.ts
@@ -1,9 +1,2 @@
-import fs from 'node:fs/promises';
-
 export function solve(input: string) {
-}
-
-if (process.env.NODE_ENV !== 'test') {
-  const input = await fs.readFile(import.meta.dirname + '/input.txt', 'utf-8');
-  console.log(solve(input));
 }
